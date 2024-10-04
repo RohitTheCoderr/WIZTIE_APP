cb5f603d9a0961bd5122d4d84759a285f37774cb
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2024-10-04 15:00:47 +0530

    correction

--- IITD_APP/src/components/sideProfile/SideProfile.jsx
+++ IITD_APP/src/components/sideProfile/SideProfile.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from '../form'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { useAuthStore, useGetProfileUserdata, useGetUserdata } from '@/services/zustandStore';
 
 function SideProfile() {
 
     const { removeToken, token } = useAuthStore((state) => state);
     const isLoggedIn = !!token;
-    
+
     const { userData } = useGetUserdata((state) => state);
 
     const { name, phoneNumber, email } = userData?.data || {};
@@ -20,19 +20,21 @@ function SideProfile() {
         <div className='font-semibold mt-4 '>
             <div className='text-gray-800'>Profile</div>
             <div className=' flex gap-2 md:gap-3 items-center  my-3 '>
-                <div className='h-12 w-12 rounded-full bg-black  sm:m-0'>
-                    <img
-                        src={Array.isArray(withimg?.profileImg) && withimg?.profileImg[0]
-                            ? `data:${withimg?.profileImg[0]?.contentType};base64,${withimg?.profileImg[0]?.data}`
-                            : null}
-                        alt={withimg?.fullName}
-                        className=' h-full w-full rounded-full object-cover'
-                    />
-                </div>
-                <div>
-                    <h5 className='font-semibold text-[12px] uppercase'>{withimg?.name}</h5>
-                    <p className='text-[10px] sm:text-[12px] text-gray-600'>Admin</p>
-                </div>
+                <Link to="/main_profile/user_account" className=' flex gap-2 md:gap-3 items-center  my-3'>
+                    <div className='h-12 w-12 rounded-full bg-black  sm:m-0'>
+                        <img
+                            src={Array.isArray(withimg?.profileImg) && withimg?.profileImg[0]
+                                ? `data:${withimg?.profileImg[0]?.contentType};base64,${withimg?.profileImg[0]?.data}`
+                                : null}
+                            alt={withimg?.fullName}
+                            className=' h-full w-full rounded-full object-cover'
+                        />
+                    </div>
+                    <div>
+                        <h5 className='font-semibold text-[12px] uppercase'>{withimg?.name}</h5>
+                        <p className='text-[10px] sm:text-[12px] text-gray-600'>Admin</p>
+                    </div>
+                </Link>
             </div>
             <div className='hover:text-[#fafafa] bg-[#55AD9B] hover:bg-[#3e9180] text-[12px] sm:text-lg h-6 sm:h-8 flex justify-center items-center rounded-sm sm:rounded-lg'>
                 {isLoggedIn ? (
